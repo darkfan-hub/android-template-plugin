35d3f3b50c252402936dfb3b06227e31586cae5d
GitHub Action <action@github.com>
2022-04-13 16:16:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/darkfanhub/androidtemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.darkfanhub.androidtemplateplugin.services
+
+import com.github.darkfanhub.androidtemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
